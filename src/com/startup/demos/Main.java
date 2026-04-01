a131203b1ce9c3958882450d29a9bfb726c3fb42
niccko1082-ux <niccko1082@gmail.com>
2026-04-01 08:22:30 -0500

    feat: add switch funcionality java 8, Java 17+

--- src/com/startup/demos/Main.java
+++ src/com/startup/demos/Main.java
@@ -3,6 +3,7 @@ package com.startup.demos;
 import com.startup.models.Empleado;
 import com.startup.models.EmpresaRecord;
 import com.startup.services.EmpleadoService;
+import java.util.Scanner;
 
 /**
  * Clase de demostración.
@@ -17,13 +18,13 @@ public class Main {
                 Encabezado del sistema""");
 
         // --- Demo: Tipos de datos primitivos ---
-        byte   n1 = 120;
-        short  n2 = 30000;
-        int    n3 = 900000000;
-        long   n4 = 1000000000000000000L;
-        float  n5 = 20.15f;
+        byte n1 = 120;
+        short n2 = 30000;
+        int n3 = 900000000;
+        long n4 = 1000000000000000000L;
+        float n5 = 20.15f;
         double n6 = 20.15;
-        char   l1 = 'n';
+        char l1 = 'n';
         boolean isTrue = true;
 
         System.out.println("Primitivos -> byte: " + n1 + ", short: " + n2 +
@@ -42,8 +43,8 @@ public class Main {
          * memoria (referencia en HEAP) y no el contenido. Por eso dos
          * objetos distintos con los mismos valores nunca son == entre sí.
          */
-        Empleado emp1 = new Empleado(100, 2000000, 10000, 90, 20, 1, false);
-        Empleado emp2 = new Empleado(101, 1800000, 8000,  80, 25, 2, true);
+        Empleado emp1 = new Empleado(100, 200, 10000, 90, 20, 1, false);
+        Empleado emp2 = new Empleado(101, 1800000, 8000, 80, 25, 2, true);
 
         emp1 = null;
 
@@ -60,6 +61,46 @@ public class Main {
         // --- Demo: EmpleadoService ---
         EmpleadoService service = new EmpleadoService();
         emp1 = emp2; // reasignamos para continuar la demo
+
+        // --- Demo: Menu Switch ---
+        Scanner sc = new Scanner(System.in);
+        int opciones;
+
+        do{
+        System.out.println("""
+                ======Menu Principal======
+                1) Calcular Salaario Final.
+                2) Verificar bono por ID par.
+                3) Validar Elegibilidad.
+                4) Mostrar Categoria Salario
+                0) Salir.
+                =========================""");
+
+        opciones = sc.nextInt();
+
+            switch (opciones){
+                case 1:
+                    System.out.println("Salario " + service.calcularSalarioFinal(emp1));
+                    break;
+                case 2:
+                    System.out.println("Bono par " + service.tieneBonoPorIdPar(emp1));
+                    break;
+                case 3:
+                    System.out.println("Elegible " + service.validarElegibilidad(emp1));
+                    break;
+                case 4:
+                    System.out.println("Categaria: " + service.obtenerCategoriaSalarial(emp1));
+                default:
+                    System.out.println("Opcion invalida");
+
+            }
+        }while(opciones != 0);
+
+        /*
+        * La diferencia entre usar switch legacy(Java 8) y switch modern(Java 17+)
+        * es que la legacy al momento de olvidar escribir break, pasa al siguiente case, ejecuta lo que tenga y
+        * asi hasta encontrar un brake, lo que genera resultados inespetados. En la Opcion creada para Java 17
+        * en adelante, cada case al terminar, tiene su propio break sin tener que ponerlo*/
 
         System.out.println("Salario final:       " + service.calcularSalarioFinal(emp1));
         System.out.println("Bono por ID par:     " + service.tieneBonoPorIdPar(emp1));
--- src/com/startup/services/EmpleadoService.java
+++ src/com/startup/services/EmpleadoService.java
@@ -27,4 +27,12 @@ public class EmpleadoService {
     public boolean validarElegibilidad(Empleado emp) {
         return (emp.puntajeTest > 85 && emp.edad < 30) || (emp.idSede == 1 && !emp.esActivo);
     }
+
+    public String obtenerCategoriaSalarial(Empleado emp){
+       return switch ((Integer) (int) emp.salarioBase) {
+            case Integer s when s < 500 -> "Junior";
+            case Integer s when s < 1000 -> "Mid";
+            default -> "Seniority";
+        };
+    }
 }
